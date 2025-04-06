615bbd5f7033d4f59565ba3d051f62a177270712
binarycraft01 <binarycraft01@gmail.com>
2025-04-06 22:20:56 +0900

    첫번째 커밋

---
+++ src/main/java/com/jibbo/jibboapp/JibboappApplication.java
@@ -0,0 +1,13 @@
+package com.jibbo.jibboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibboappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibboappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibbo/jibboapp/JibboappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibbo.jibboapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibboappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

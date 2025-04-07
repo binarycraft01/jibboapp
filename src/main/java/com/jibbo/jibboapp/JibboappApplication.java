4fc5db06c6edea90ea14f0a16a26ca8faa9e9563
binarycraft01 <binarycraft01@gmail.com>
2025-04-07 21:32:41 +0900

    통신 에러 수정

--- src/main/java/com/jibbo/jibboapp/JibboappApplication.java
+++ src/main/java/com/jibbo/jibboapp/JibboappApplication.java
@@ -2,8 +2,10 @@ package com.jibbo.jibboapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.jibbo.jibboapp.repository")
 public class JibboappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jibbo/jibboapp/domain/User.java
+++ src/main/java/com/jibbo/jibboapp/domain/User.java
@@ -3,6 +3,7 @@ package com.jibbo.jibboapp.domain;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id

636245e6ac4c9b85c8d014dba2f2e20e88e4874e
binarycraft01 <binarycraft01@gmail.com>
2025-04-12 19:12:03 +0900

    하우스 도메인 생성

---
+++ src/main/java/com/jibbo/jibboapp/domain/House.java
@@ -0,0 +1,40 @@
+package com.jibbo.jibboapp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class House {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;          // 집 제목
+    private String realtorName;   // 부동산 이름
+    private String dealType;      // "전세", "월세", "매매"
+    private Long deposit;         // 보증금
+    private Long monthlyRent;     // 월세
+    private Long salePrice;       // 매매가
+    private String address;       // 주소
+    private String memo;          // 메모
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")  // 외래키
+    private User user;
+
+    protected House() {}
+
+    public House(String title, String realtorName, String dealType, Long deposit, Long monthlyRent, Long salePrice, String address, String memo, User user) {
+        this.title = title;
+        this.realtorName = realtorName;
+        this.dealType = dealType;
+        this.deposit = deposit;
+        this.monthlyRent = monthlyRent;
+        this.salePrice = salePrice;
+        this.address = address;
+        this.memo = memo;
+        this.user = user;
+    }
+
+    // Getter, Setter 생략 or Lombok 사용 가능
+}
